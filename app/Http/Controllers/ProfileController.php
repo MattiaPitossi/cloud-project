af81e3f4d0406a888364944602f8f86d5f7b04f1
MattiaPitossi <mattiapitossi@icloud.com>
2021-11-29 19:42:17 +0100

    Add ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileController extends Controller
+{
+    public function getUserProfile()
+    {
+        $user_id = Auth::user()->id;
+
+
+        $user_data = DB::table('users')->where('id','=',$user_id)->get();
+        return view('profile', compact('user_data'));
+    }
+
+    public function update(Request $request, $id, $name)
+    {
+        //dd($request);
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+
+        $user_id = Auth::user()->id;
+
+        return redirect()->route('index')
+            ->with('success', 'Profile updated successfully');
+    }
+}

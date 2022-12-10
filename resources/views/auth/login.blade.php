a07d39c9287ac10a76307c98b100624c0071b355
Mattia <mattiapitossi@icloud.com>
2022-12-10 17:00:11 +0100

    Add placeholder for password in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                <x-input id="password" placeholder="Enter your password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="current-password" />
             </div>
 
